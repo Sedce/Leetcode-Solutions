69a42b6392835dc615085183d21288d391761073
Cedez <cggulane@up.edu.ph>
2024-01-23 21:38:30 -0700

    Leetcode romanToInt solution

---
+++ java/src/romanToInt.java
@@ -0,0 +1,16 @@
+
+public class romanToInt {
+
+        int result = 0;
+        int[] decimal = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < decimal.length; i++ ) {
+            while (s.indexOf(roman[i]) == 0) {
+                result += decimal[i];
+                s = s.substring(roman[i].length());
+            }
+        }
+        return result;
+
+}
